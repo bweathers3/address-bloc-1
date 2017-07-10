4fae12a8c3b820adc95d7bd24d8c348a092b9d7d
Bailey <bailey.g.weathers@gmail.com>
2017-07-09 21:18:55 -0500

    Complete 07-checkpoint-associations-addressBloc

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,13 +5,10 @@ require 'bloc_record/base'
 
 class AddressBook < BlocRecord::Base
 
+  has_many :entries
 
   def add_entry(name, phone_number, email)
     Entry.create(name: name, phone_number: phone_number, email: email)
-  end
-
-  def entries
-    Entry.where(address_book_id: self.id)
   end
 
   def find_entry(name)
